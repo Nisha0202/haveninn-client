49f5273bf44952d48c1fac432b191d0d9fe59628
Nisha0202 <nishajabatunnessa@gmail.com>
2024-05-12 15:04:14 +0600

    sort toggle

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import Card from '../components/Card';
+
+import { AuthContext } from '../FirebaseProbider/FirbaseProvider'
+
+
 
 export default function Bookings() {
+const { usern } = useContext(AuthContext);
+
+    const [data, setData] = useState([]);
+    useEffect(() => {
+      fetch(`http://localhost:5000/rooms/user?email=${usern.email}`)
+        .then(response => response.json())
+        .then(data => setData(data))
+        .catch(error => console.error('Error:', error));
+    }, [usern]);
+    
+    
+  
+  
   return (
-    <div>Bookings</div>
+    <div className='flex justify-center items-center pb-12'>
+         <div className='flex flex-wrap flex-col gap-4 md:flex-row md:justify-between lg:justify-between md:gap-16'>
+
+{data.map((estate, index) => (
+        <Card key={index} estate={estate} />
+      ))}
+    </div>
+    </div>
+   
   )
 }
--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,24 +1,33 @@
-import React, { useEffect } from 'react'
-import { useState } from "react";
+import React, { useEffect, useState } from 'react';
 import Card from '../components/Card';
 
 export default function Rooms() {
   const [data, setData] = useState([]);
+  const [isSorted, setIsSorted] = useState(false);
 
-  const fetchData = () => {
-   // Fetch sorted data
-fetch('http://localhost:5000/rooms?sort=price')
-.then(response => response.json())
-.then(data => setData(data))
-.catch(error => console.error('Error:', error));
-
+  const fetchData = (sort = false) => {
+    let url = 'http://localhost:5000/rooms';
+    if (sort) {
+      url += '?sort=price';
+    }
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error:', error));
   };
 
-  useEffect(fetchData, []);
+  const toggleSort = () => {
+    fetchData(!isSorted);
+    setIsSorted(!isSorted);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className='flex flex-col justify-center items-center pb-12'>
-      <div className='btn btn-sm mb-8' onClick={fetchData}>Sort by Price</div>
+      <div className='btn btn-sm mb-8' onClick={toggleSort}>Sort by Price</div>
       <div className='flex flex-wrap flex-col gap-4 md:flex-row md:justify-between lg:justify-between md:gap-16'>
         {data.map((estate, index) => (
           <Card key={index} estate={estate} />
@@ -27,3 +36,4 @@ fetch('http://localhost:5000/rooms?sort=price')
     </div>
   )
 }
+
